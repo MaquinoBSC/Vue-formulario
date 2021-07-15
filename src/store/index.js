2727ee9d5985bdd53b3be07b35d3c5427bf25654
MaquinoBSC <martinare.ipn@gmail.com>
2021-07-14 23:15:59 -0500

    Enviar tareas a firebase consumiendo la API

--- src/store/index.js
+++ src/store/index.js
@@ -18,13 +18,11 @@ export default createStore({
     },
     set(state, payload){
       state.tareas.push(payload);
-      localStorage.setItem('tareas', JSON.stringify(state.tareas));
     },
     eliminar(state, payload){
       state.tareas= state.tareas.filter((tarea)=> {
         return tarea.id !== payload;
       })
-      localStorage.setItem('tareas', JSON.stringify(state.tareas));
     },
     get(state, payload){
       if(!state.tareas.find((tarea)=> tarea.id === payload)){//Validacion por si el id no existe
@@ -39,21 +37,30 @@ export default createStore({
       state.tareas= state.tareas.map((tarea)=> {
         return tarea.id == payload.id ? payload : tarea;
       });
-      localStorage.setItem('tareas', JSON.stringify(state.tareas));
       router.push('/');
     }
   },
   actions: {
     cargarLocalStorage({commit}){
-      if(localStorage.getItem('tareas')){
-        const tareas= JSON.parse(localStorage.getItem('tareas'))
-        commit('cargar', tareas)
-        return
+      
+    },
+    async setTareas({commit}, tarea){
+      try {
+        const res= await fetch(`https://vue-formulario-885d5-default-rtdb.firebaseio.com/tareas/${tarea.id}.json`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(tarea)
+        })
+
+        const dataDB= await res.json()
+        console.log(dataDB);
+
+      } catch (error) {
+        console.log(error);
       }
 
-      localStorage.setItem('tareas', JSON.stringify([]));
-    },
-    setTareas({commit}, tarea){
       commit('set', tarea)
     },
     deleteTarea({commit}, id){
