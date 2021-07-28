a0c304a45b4bf78e9c3043c5e940f513338f3340
MaquinoBSC <martinare.ipn@gmail.com>
2021-07-28 09:52:57 -0500

    rutas protegidas

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import store from '../store';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {rutaProtegida: true}
   },
   {
     path: '/editar/:id',
@@ -15,7 +17,8 @@ const routes = [
   {
     path: '/registro',
     name: 'Registro',
-    component: ()=> import('../views/Registro.vue')
+    component: ()=> import('../views/Registro.vue'),
+    meta: {rutaProtegida: true}
   },
   {
     path: '/ingreso',
@@ -29,4 +32,17 @@ const router = createRouter({
   routes
 })
 
+router.beforeEach((to, from, next)=> {//beforeeach hace un recorrido por cada una de nuestras rutas
+  if(to.meta.rutaProtegida){//para todas las rutas que sean protegidas va a mandar true
+    if(store.getters.usuarioAutenticado){//Si el usuraio es autenticado
+      next();//Podemos acceder a la ruta protejida
+    }
+    else{
+      next('/ingreso');//De lo contrario lo mandamos a ingreso
+    }
+  }
+  else{
+    next();//si la ruta no es protegida hacemos el next, que efectivamente vaya a esa ruta con seguridad
+  }
+})
 export default router
